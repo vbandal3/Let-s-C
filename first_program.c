70d266a4a9da3d2cec23d07b72f66feb33e2c6a8
Vaibhav Bandal <93034627+vbandal3@users.noreply.github.com>
2021-12-01 14:21:08 +0530

    First program in C
    This is very first program in C. How to print a statement.

---
+++ first_program.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    printf("Hello Smita");
+    return 0;
+}
+
+// #include<stdio.h>
+// int main()
+// {int num1,num2,sum;
+// printf("Enter first number",&num1);  
+// scanf("%d",num1);
+// printf("Enter second number",&num2);
+// scanf("%d",&num2);
+// sum=num1+num2;
+// printf("Sum of numbers is %d",sum);     
+// return 0 ;  
+// }
